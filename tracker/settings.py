4e5c957e951300bf3101955ca655b8ce23561ab6
Elliot Marsden <elliot.marsden@gmail.com>
2016-01-16 02:33:29 +0000

    Add foundation to crispy layout pack
    Annoying that we have to re-state the original values here,
    it would be nice if crispy made its defaults more easily available.

--- tracker/settings.py
+++ tracker/settings.py
@@ -141,6 +141,10 @@ if DEBUG:
     CSP_STYLE_SRC = ("'self'", "'unsafe-inline'")
     CSP_IMG_SRC += ("*.placehold.it",)
 
+# Add 'foundation-5' layout pack
+CRISPY_ALLOWED_TEMPLATE_PACKS = ('bootstrap', 'uni_form', 'bootstrap3',
+                                 'foundation-5')
+
 CRISPY_TEMPLATE_PACK = 'foundation-5'
 
 AUTH_USER_MODEL = 'djangae.GaeUser'
